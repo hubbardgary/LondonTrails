997ceefc6f398963f85af609daa328d463b3e695
Gary Hubbard <hubbardgary@users.noreply.github.com>
2024-07-13 12:13:00 +0100

    More package updates

--- app/src/main/java/com/hubbardgary/londontrails/proxy/AndroidFrameworkProxy.java
+++ app/src/main/java/com/hubbardgary/londontrails/proxy/AndroidFrameworkProxy.java
@@ -2,9 +2,7 @@ package com.hubbardgary.londontrails.proxy;
 
 import android.text.Html;
 
-import com.google.android.gms.common.GoogleApiAvailability;
 import com.hubbardgary.londontrails.proxy.interfaces.IAndroidFrameworkProxy;
-import com.hubbardgary.londontrails.view.interfaces.IGoogleMapsLicenceView;
 
 public class AndroidFrameworkProxy implements IAndroidFrameworkProxy {
     @Override
